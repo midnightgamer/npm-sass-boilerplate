ee9f893695b1ae84a2980994a336eff13152e26d
Pnkaj Jaiswal <jaiswal1999.pj@gmail.com>
2019-06-06 06:54:48 -0700

    After build script content will be placed in src folder

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const fs = require('fs-extra');
+const dir = 'src';
+
+try {
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir)
+    }
+} catch (err) {
+    console.error(err)
+}
+
+try {
+    fs.copy('index.html','src/index.html',function (err) {
+        if (!err){
+            console.log("Success")
+        }
+    });
+}catch (e) {
+    console.log(e)
+}
