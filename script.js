0902d3279997674b8bef1fb748874ed37855c7a4
Pnkaj Jaiswal <jaiswal1999.pj@gmail.com>
2019-06-11 03:32:23 -0700

    Copy btn bug resolved

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ function copyToClipboard(el) {
     var range = document.createRange();
     range.selectNode(emailLink);
     window.getSelection().addRange(range);
-
     try {
         // Now that we've selected the anchor text, execute the copy command
         var successful = document.execCommand('copy');
         var msg = successful ? 'successful' : 'unsuccessful';
         console.log('Copy command was ' + msg);
-        document.querySelector('.tooltip').style.display = 'block';
+        document.querySelector('.tooltip').classList.add('u-appear');
         setTimeout(function () {
-            document.querySelector('.tooltip').classList.add('u-disappear')
+            document.querySelector('.tooltip').classList.remove('u-appear');
+            document.querySelector('.tooltip').classList.add('u-disappear');
         },2000)
     } catch(err) {
         console.log('Oops, unable to copy');
     }
+    window.getSelection().removeAllRanges();
 };
