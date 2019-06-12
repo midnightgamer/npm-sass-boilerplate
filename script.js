ffa3c4b1f8ceda6c106fbc77acf82d7e4c55d79d
Pnkaj Jaiswal <jaiswal1999.pj@gmail.com>
2019-06-12 05:41:10 -0700

    Navigation active on scroll and click

--- script.js
+++ script.js
@@ -13,9 +13,46 @@ function copyToClipboard(el) {
         setTimeout(function () {
             document.querySelector('.tooltip').classList.remove('u-appear');
             document.querySelector('.tooltip').classList.add('u-disappear');
-        },2000)
-    } catch(err) {
+        }, 2000)
+    } catch (err) {
         console.log('Oops, unable to copy');
     }
     window.getSelection().removeAllRanges();
 };
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        //TODO preventDefault()
+        e.defaultPrevented()
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+        var target = this.hash;
+        menu = target;
+        $target = $(target);
+        $('html', 'body').stop().animate({}, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('#side-nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#side-nav ul li a').removeClass("active");
+            currLink.addClass("active");
+            console.log("scolll")
+        } else {
+            currLink.removeClass("active");
+        }
+    });
+}
