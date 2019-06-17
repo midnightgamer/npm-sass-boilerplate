d2df2e4ec5bfe3bfca8953401052fe6bb191a855
Pnkaj Jaiswal <jaiswal1999.pj@gmail.com>
2019-06-17 11:41:54 -0700

    Feature minifing anf concating all files now

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,15 @@
 const fs = require('fs-extra');
 const globby = require('globby');
-const compressor = require('node-minify');
+// const compressor = require('node-minify');
 const concat = require('concat');
+var CleanCSS = require('clean-css');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
 
 const Srcdir = 'src';
 try {
     if (!fs.existsSync(Srcdir)) {
-        fs.mkdirSync(Srcdir)
+        fs.mkdirSync(Srcdir);
         //Creating new css folder inside src directory
         const cssFile = Srcdir+'/css';
         fs.mkdirSync(cssFile,function (err) {
@@ -30,9 +33,26 @@ try {
             concat(paths).then(result =>
                 fs.outputFile('src/css/style.css', result, function(err) {
                     console.log(err); //null
-                })
+                    var minifiedCSS = new CleanCSS().minify(result);
+                   console.log(minifiedCSS.styles);
+
+                   fs.outputFile('src/css/style.min.css',minifiedCSS.styles,function (e) {
+                       console.log(e)
+                   })
+                }),
             )
         });
+    //TODO Prefixadder
+
+    //    Adding Prefixes
+    //     const css = cssFile+'/style.css';
+    //     console.log(css)
+    //     postcss([ autoprefixer ]).process(css).then(result => {
+    //         result.warnings().forEach(warn => {
+    //             console.warn(warn.toString())
+    //         })
+    //         console.log(result.css)
+    //     })
     }
 } catch (err) {
     console.error(err)
