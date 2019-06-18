fa0b0dfea14d729e978b35851c70173f666611ed
Pnkaj Jaiswal <jaiswal1999.pj@gmail.com>
2019-06-18 09:41:50 -0700

    Prefixing and minifing css and cleaned build file

--- scripts/build.js
+++ scripts/build.js
@@ -6,19 +6,33 @@ const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 
 const Srcdir = 'src';
+const cssFile = Srcdir+'/css';
+const jsFile = Srcdir+'/js';
+
 try {
-    if (!fs.existsSync(Srcdir)) {
-        fs.mkdirSync(Srcdir);
+    //Create new Src directory if not created
+    if (!fs.existsSync(Srcdir)){
+        fs.mkdirSync(Srcdir)
+    }
+    if(fs.existsSync(Srcdir)){
         //Creating new css folder inside src directory
-        const cssFile = Srcdir+'/css';
+
         fs.mkdirSync(cssFile,function (err) {
             console.log(err)
         });
         //Creating new js folder inside src directory
-        const jsFile = Srcdir+'/js';
         fs.mkdirSync(jsFile,function (err) {
             console.log(err)
         });
+    }
+
+}
+catch (e) {
+    console.log(e.message)
+}
+
+try{
+    if (fs.existsSync(Srcdir)){
 
         //Copying Html file to src
         //TODO implement copy not only for index.html but all html files
@@ -27,20 +41,44 @@ try {
                 console.log('\x1b[32m%s\x1b[0m', 'Transferring your all file to src....');
             }
         });
-        //Concat All files from css directory to one css file
-        globby(['css/*.css', '!node_modules']).then(paths => {
-            concat(paths).then(result =>
-                fs.outputFile('src/css/style.css', result, function(err) {
-                    console.log(err); //null
-                    var minifiedCSS = new CleanCSS().minify(result);
-                   console.log(minifiedCSS.styles);
 
-                   fs.outputFile('src/css/style.min.css',minifiedCSS.styles,function (e) {
-                       console.log(e)
-                   })
-                }),
-            )
-        });
+        globby(['css/*.css','!node_modules']).then(paths =>{
+            concat(paths).then(result =>{
+                postcss([ autoprefixer ]).process(result).then(preFixed => {
+                    preFixed.warnings().forEach(warn => {
+                        console.warn(warn.toString())
+                    });
+                    fs.outputFile(cssFile +'/style.css',preFixed.css);
+                    var minifedCSS = new CleanCSS().minify(preFixed.css);
+                    fs.outputFile(cssFile+'/style.min.css',minifedCSS.styles);
+                });
+
+            });
+        })
+    }
+}
+catch (e) {
+    console.log(e.message)
+}
+
+try {
+    if (!fs.existsSync(Srcdir)) {
+        fs.mkdirSync(Srcdir);
+
+        //Concat All files from css directory to one css file
+        // globby(['css/*.css', '!node_modules']).then(paths => {
+        //     concat(paths).then(result =>
+        //         fs.outputFile('src/css/style.css', result, function(err) {
+        //             console.log(err); //null
+        //             var minifiedCSS = new CleanCSS().minify(result);
+        //            console.log(minifiedCSS.styles);
+        //
+        //            fs.outputFile('src/css/style.min.css',minifiedCSS.styles,function (e) {
+        //                console.log(e)
+        //            })
+        //         }),
+        //     )
+        // });
 
         //TODO Working postCSS Autoprefixer
 
